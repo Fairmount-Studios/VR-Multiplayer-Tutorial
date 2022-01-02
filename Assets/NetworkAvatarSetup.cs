0a0cb265e180e5b6107d567dd6a99735c102d7d8
Kamy Moussavi <32441857+KamyMouss@users.noreply.github.com>
2022-01-02 13:59:57 -0500

    Revert "Revert "network avatar with locomotion + bug""
    This reverts commit a5d92cd05d13563f56496aca30604804afacdd10.

--- Assets/NetworkAvatarSetup.cs
+++ Assets/NetworkAvatarSetup.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 using Photon.Pun;
 using UnityEngine.XR.Interaction.Toolkit;
 using TMPro;
+using Oculus.Avatar2;
 public class NetworkAvatarSetup : MonoBehaviourPunCallbacks
 {
     public GameObject LocalAvatar;
     public GameObject RemoteLoopbackAvatar;
-    public GameObject AvatarSdkManager;
-    public GameObject LipSyncInput;
+    //    public GameObject AvatarSdkManager;
+    //    public GameObject LipSyncInput;
+    //public GameObject XROrigin;
 
     public TextMeshProUGUI PlayerName_Text;
+
+    //Meta Avatars Tracking Input
+    private OvrAvatarEntity _localAvatarEntity;
+    private OvrAvatarBodyTrackingBehavior _bodyTracking;
+    private OvrAvatarLipSyncBehavior _lipSync;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +29,16 @@ public class NetworkAvatarSetup : MonoBehaviourPunCallbacks
             //The player is local
             LocalAvatar.SetActive(true);
             RemoteLoopbackAvatar.SetActive(false);
-            AvatarSdkManager.SetActive(true);
-            LipSyncInput.SetActive(true);
+            //XROrigin.SetActive(true);
+
+
+            _localAvatarEntity = LocalAvatar.GetComponent<SampleAvatarEntity>();
+            _bodyTracking = GameObject.Find("AvatarSdkManagerHorizon").GetComponent<SampleInputManager>();
+            if (_bodyTracking == null)
+            { Debug.LogError("Body Tracking is null"); }
+            _lipSync = GameObject.Find("LipSyncInput").GetComponent<OvrAvatarLipSyncContext>();
+            _localAvatarEntity.SetBodyTracking(_bodyTracking);
+            _localAvatarEntity.SetLipSync(_lipSync);
 
         }
         else
@@ -31,8 +46,8 @@ public class NetworkAvatarSetup : MonoBehaviourPunCallbacks
             //The player is remote
             LocalAvatar.SetActive(false);
             RemoteLoopbackAvatar.SetActive(true);
-            AvatarSdkManager.SetActive(false);
-            LipSyncInput.SetActive(false);
+            //XROrigin.SetActive(false);
+
 
         }
 
